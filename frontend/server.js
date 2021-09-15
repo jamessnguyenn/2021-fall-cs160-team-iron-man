f249b189b0a22ee918a330fa84ec1c397c9a3f42
James Nguyen <nguyen.james615@gmail.com>
2021-09-15 11:52:12 -0700

    added frontend server and file structure

---
+++ frontend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+
+const app = express();
+const port = 3000;
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch("./");
+
+app.use(connectLivereload());
+
+//Set the localhost:3000/ page to load home.html
+app.get('/', (req, res) => {
+    res.sendFile('./html/home.html', { root: __dirname });
+});
+app.get('/job-seeker-login', (req, res) => {
+    res.sendFile('./html/job-seeker-login.html', { root: __dirname });
+});
+
+
+app.listen(port, () => console.log(`launched web server on port 3000`));

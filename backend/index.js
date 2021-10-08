cc9456afeb4f84dd8eb611023039c85b076d647e
James Nguyen <nguyen.james615@gmail.com>
2021-10-08 12:31:05 -0700

    added authentication API for Jobseekers

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.json());
 app.use(cors());
 
--- backend/routes/jobseekers.js
+++ backend/routes/jobseekers.js
@@ -1,17 +1,61 @@
-const User = require('../models/jobseeker.model')
+const JobSeeker= require('../models/jobseeker.model')
 const router = require('express').Router()
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authenticateToken = require('./authenticateToken');
 
 router.route('/').post((req, res)=>{
     const BCRYPT_SALT_ROUNDS = 12;
     const{firstName, lastName, email, password, address, experiences, websites, education} = req.body;
     const hashedPassword = bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS);   
-    const newUser = new User({firstName, lastName, hashedPassword, email, address, experiences, websites, education});
+    const newUser = new JobSeeker({firstName, lastName, hashedPassword, email, address, experiences, websites, education});
     newUser.save()
-    .then(user=>{
-        console.log("Sucessfully added the user: " + user._id)
-        res.status(200).json({user_id: user._id})})
-    .catch(err=>res.status(400).json({error: err}))
+    .then(user =>{
+        jwt.sign({
+            user_id: user._id,
+        },
+        process.env.JWT_SECRET,
+        (err, token) =>{
+            if(err){ 
+                res.status(410).json({error: "Account is created. But you are not signed in. Please Try again."});
+            }else{
+                res.json({token, user_id: user._id});
+            }
+        })
+    })
+    .catch(err=>{
+        if(err.code === 11000){
+            res.status(410).json({msg: "Email already exists"})
+        }else{
+            res.status(400).json(err)
+        }
+    })
+})
+
+router.route('/auth').post((req, res)=>{
+    const{ email, password} = req.body;
+    if(!email || !password){
+        return res.status(410).json({msg: 'Missing Required Fields'});
+    }
+    JobSeeker.findOne({email})
+        .then(user =>{
+            if(!user) return res.status(402).json({msg: 'Invalid email or password'});
+            bcrypt.compare(password, user.hashedPassword)
+                .then(matches =>{
+                    if(!matches) return res.status(402).json({msg: 'Invalid email or password'});
+                    jwt.sign({
+                        user_id: user._id,
+                    },
+                    process.env.JWT_SECRET,
+                    (err, token) =>{
+                        if(err) throw err;
+                        res.json({token, user_id: user._id});
+                    })
+                })
+        })
+        .catch(err=>{
+            res.status(400).json(err)
+        })
 })
 
 module.exports = router;

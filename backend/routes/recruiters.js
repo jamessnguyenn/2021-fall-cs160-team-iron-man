6172399345cb326ea7f1a68c98604f5b57744fe4
Christophervtran92 <Christophervtran92@gmail.com>
2021-10-23 17:56:07 -0700

    Changed User/user/doc to Recruiter/recruiter for better understanding. Changed 401 Unauthorized -> 403 Forbidden. Removed password change from put. Removed testing output from put. Updated other outputs to be in-line with backend-api document. Removed unused variables.

--- backend/routes/recruiters.js
+++ backend/routes/recruiters.js
@@ -1,22 +1,20 @@
-const User = require('../models/recruiter.model')
+const Recruiter = require('../models/recruiter.model')
 const router = require('express').Router()
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authenticateToken = require('./authenticateToken');
-const { query } = require('express');
-const { off } = require('../models/recruiter.model');
 
 //Create a new recruiter
 router.route('/').post((req, res) => {
     const BCRYPT_SALT_ROUNDS = 12;
     const{firstName, lastName, email, password, companyName, logoLink, companyDescription, companyWebsite} = req.body;
     const hashedPassword = bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS);
-    const newUser = new User({firstName, lastName, hashedPassword, email, companyName, logoLink, companyDescription, companyWebsite});
+    const newRecruiter = new Recruiter({firstName, lastName, hashedPassword, email, companyName, logoLink, companyDescription, companyWebsite});
 
-    newUser.save()
-    .then(user=>{
+    newRecruiter.save()
+    .then(recruiter=>{
         jwt.sign({
-            user_id: user._id,
+            user_id: recruiter._id,
             role: "recruiter"
         },
         process.env.JWT_SECRET,
@@ -24,7 +22,8 @@ router.route('/').post((req, res) => {
             if(err){
                 res.status(419).json({error: "Error generating token"});
             }else{
-                res.status(200).json({token, user_id: user._id});
+                //res.status(200).json({token, user_id: recruiter._id});            //Use this line to get token printout
+                res.status(200).json('OK')
             }
         })
     })
@@ -37,106 +36,60 @@ router.route('/').post((req, res) => {
     })
 })
 
-//Authenticate a recruiter
-router.route('/auth').post((req, res)=>{
-    const{ email, password} = req.body;
-    if(!email || !password){
-        return res.status(400).json({msg: 'Missing Required Fields'});
-    }
-    User.findOne({email})
-        .then(user =>{
-            if(!user) return res.status(401).json({msg: 'Invalid email or password'});
-            bcrypt.compare(password, user.hashedPassword)
-                .then(matches =>{
-                    if(!matches) return res.status(401).json({msg: 'Invalid email or password'});
-                    jwt.sign({
-                        user_id: user._id,
-                        role: "recruiter"
-                    },
-                    process.env.JWT_SECRET,
-                    (err, token) =>{
-                        if(err){ 
-                            res.status(419).json({error: "Error Generating Token"});
-                        }else{
-                            res.json({token, user_id: user._id});
-                        }
-                    })
-                })
-        })
-        .catch(err=>{
-            res.status(400).json(err)
-        })
-})
-
 //Update a recruiter
 router.route('/:id').put(authenticateToken, (req, res) => {
-    User.findById(req.params.id)
-    .then(doc=> {
-        if(!doc) {
+    Recruiter.findById(req.params.id)
+    .then(recruiter=> {
+        if(!recruiter) {
             return res.status(400).json({error: "Bad Request"});
         }
         if(req.user.user_id !== req.params.id) {
-            return res.status(401).json({error: "Unauthorized"});
+            return res.status(403).json({error: "Forbidden"});
         }
-        
-        //Checking if they are updating a password also, since we don't want to encrypt a empty password
-        if(req.body.password) {
-            const BCRYPT_SALT_ROUNDS = 12;
-            const hashedPassword = bcrypt.hashSync(req.body.password, BCRYPT_SALT_ROUNDS);
-            const filter = {_id: req.params.id};
-            const update = {firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, hashedPassword: hashedPassword,
-                companyName: req.body.companyName, logoLink: req.body.logoLink, companyDescription: req.body.companyDescription, companyWebsite: req.body.companyWebsite};
-                User.findOneAndUpdate(filter, update)
-                .then(() => res.status(200).json('Recruiter id: ' + req.params.id + ', updated')) //Testing
-                //.then(() => res.status(200).json('OK'))
-                .catch(err => res.status(400).json({error: "Bad request"}))
-        }else{
         const filter = {_id: req.params.id};
-        const update = {firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, hashedPassword: hashedPassword,
-            companyName: req.body.companyName, logoLink: req.body.logoLink, companyDescription: req.body.companyDescription, companyWebsite: req.body.companyWebsite};
-            User.findOneAndUpdate(filter, update)
-            .then(() => res.status(200).json('Recruiter id: ' + req.params.id + ', updated')) //Testing
-            //.then(() => res.status(200).json('OK'))
+        const update = {firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, companyName: req.body.companyName, 
+                        logoLink: req.body.logoLink, companyDescription: req.body.companyDescription, companyWebsite: req.body.companyWebsite};
+            Recruiter.findOneAndUpdate(filter, update)
+            .then(() => res.status(200).json('OK'))
             .catch(err => res.status(400).json({error: "Bad request"}))
-        }
     })
 })
 
 //Getting all recruiters, for testing purposes
 router.route('/').get((req, res) => {
-    User.find()
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(400).json({error: err}))
+    Recruiter.find()
+    .then(recruiter => res.status(200).json(recruiter))
+    .catch(err => res.status(400).json({error: "Bad request"}))
 })
 
 //Getting recruiters by object id
 router.route('/:id').get(authenticateToken, (req, res) => {
-    User.findById(req.params.id, '-hashedPassword')
-    .then(user => {
-        if(!user) {
+    Recruiter.findById(req.params.id, '-hashedPassword')
+    .then(recruiter => {
+        if(!recruiter) {
             return res.status(400).json({error: "Bad Request"});
         }
         if(req.user.user_id !== req.params.id) {
-            return res.status(401).json({error: "Unauthorized"});
+            return res.status(403).json({error: "Forbidden"});
         }
-        res.status(200).json(user)
+        res.status(200).json(recruiter)
     })
     .catch(err => res.status(400).json({error: "Bad request"}))
 })
 
 //Deleting a recruiter
 router.route('/:id').delete(authenticateToken, (req, res) => {
-    User.findById(req.params.id)
-    .then(doc=> {
-        if(!doc) {
+    Recruiter.findById(req.params.id)
+    .then(recruiter=> {
+        if(!recruiter) {
             return res.status(400).json({error: "Bad Request"});
         }
         if(req.user.user_id !== req.params.id) {
-            return res.status(401).json({error: "Unauthorized"});
+            return res.status(403).json({error: "Forbidden"});
         }
-        User.deleteOne({_id: req.params.id})
+        Recruiter.deleteOne({_id: req.params.id})
         .then(result=>{
-            res.status(200).json({"message": "Recruiter deleted"})
+            res.status(200).json('OK')
         })
         .catch(err=> res.status(400).json({error: "Bad request"}))
     })
@@ -147,14 +100,14 @@ router.route('/auth').post((req, res)=>{
     if(!email || !password){
         return res.status(400).json({msg: 'Missing Required Fields'});
     }
-    User.findOne({email})
-        .then(user =>{
-            if(!user) return res.status(401).json({msg: 'Invalid email or password'});
-            bcrypt.compare(password, user.hashedPassword)
+    Recruiter.findOne({email})
+        .then(recruiter =>{
+            if(!recruiter) return res.status(401).json({msg: 'Invalid email or password'});
+            bcrypt.compare(password, recruiter.hashedPassword)
                 .then(matches =>{
                     if(!matches) return res.status(401).json({msg: 'Invalid email or password'});
                     jwt.sign({
-                        user_id: user._id,
+                        user_id: recruiter._id,
                         role: "recruiter"
                     },
                     process.env.JWT_SECRET,
@@ -162,7 +115,7 @@ router.route('/auth').post((req, res)=>{
                         if(err){ 
                             res.status(419).json({error: "Error Generating Token"});
                         }else{
-                            res.json({token, user_id: user._id});
+                            res.status(200).json({user_id: recruiter._id, token});
                         }
                     })
                 })

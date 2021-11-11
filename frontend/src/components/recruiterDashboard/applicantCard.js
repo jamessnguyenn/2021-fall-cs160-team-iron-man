b70c8d064800cefbffb723c4b4af6306b3d3d347
James Nguyen <nguyen.james615@gmail.com>
2021-11-11 10:32:03 -0800

    fixed bug where experiences would return the 2nd element not the firtst for the card

--- frontend/src/components/recruiterDashboard/applicantCard.js
+++ frontend/src/components/recruiterDashboard/applicantCard.js
@@ -18,9 +18,8 @@ export default function ApplicantPage({street, apt, zip, city, state, firstName,
                   <Card.Title>{firstName} {lastName}</Card.Title>
                   <Card.Text>{city}, {state}</Card.Text>
                   <Card.Text style={{ fontWeight: "bold", color: "#4682B4", marginTop: "0px", marginBottom: "0px"}}>
-                    {experiences.length > 0? experiences[1].position : education? education.fieldOfStudy+" Student": "No Experience"}
+                    {experiences.length > 0? experiences[0].position : education? education.fieldOfStudy+" Student": "No Experience"}
                   </Card.Text>
-
                   <Card.Link
                     onClick={handleLgShow}
                     href="#"
@@ -34,9 +33,9 @@ export default function ApplicantPage({street, apt, zip, city, state, firstName,
                       <Modal.Title>{firstName} {lastName} <div style={{fontSize: "14px", color: '#777'}}>{email}</div> </Modal.Title>
                     </Modal.Header>
                     <Modal.Body><p><strong>Address</strong><div className="ms-5">{street} {apt}, {city}, {state} {zip}</div> </p>
-                    <p><strong>Experiences </strong>{experiences.length === 2 ? <div className="ms-5"> {experiences[1].position + ", " + experiences[1].company +  " (" + experiences[1].startDate + " - " +  experiences[1].endDate + ")"} <p> {experiences[0].position + ", " + experiences[0].company +  " (" + experiences[0].startDate + " - " + experiences[0].endDate + ")"} </p> </div> : experiences.length === 1 ? experiences[0].position + ", " + experiences[0].company +  " (" + experiences[0].startDate + " - " +  experiences[0].endDate + ")" : education? education.fieldOfStudy+" Student": "No Experience" }</p>
+                    <p><strong>Experiences</strong>{experiences.length === 2 ? <div className="ms-5"> {experiences[1].position + ", " + experiences[1].company +  " (" + experiences[1].startDate + " - " +  experiences[1].endDate + ")"} <p> {experiences[0].position + ", " + experiences[0].company +  " (" + experiences[0].startDate + " - " + experiences[0].endDate + ")"} </p> </div> : experiences.length === 1 ? experiences[0].position + ", " + experiences[0].company +  " (" + experiences[0].startDate + " - " +  experiences[0].endDate + ")" : education? education.fieldOfStudy+" Student": "No Experience" }</p>
                     <p><strong>Websites</strong>{websites.length === 3 ? <div className="ms-5"> {websites[0]} <div>{websites[1]}</div> <div>{websites[2]} </div> </div> : websites.length === 2 ? websites[0] + ", " + websites[1] : websites.length === 1 ? websites[0] : null  } </p>
-                    <div><strong>Education </strong><div className="ms-5">{education.school} <em style={{fontWeight: "lighter"}}>(Class Of {education.endDate}), </em> {education.degree} degree, {education.fieldOfStudy}</div></div>       
+                    <div><strong>Education</strong><div className="ms-5">{education.school} <em style={{fontWeight: "lighter"}}>(Class Of {education.endDate}), </em> {education.degree} degree, {education.fieldOfStudy}</div></div>       
                     </Modal.Body>
                     
                     <Modal.Footer>

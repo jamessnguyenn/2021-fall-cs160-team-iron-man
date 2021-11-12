4da7b62e014c8dd52494bdf5a8c3a4d9037d5b95
Christophervtran92 <Christophervtran92@gmail.com>
2021-11-11 16:06:09 -0800

    Added frontend test that logs in a recruiter, filters jobs, clicks learn more on a applicant, and checks for their email

--- test-automation/EZApply_Frontend_Tests/src/Main.java
+++ test-automation/EZApply_Frontend_Tests/src/Main.java
@@ -20,12 +20,14 @@ public class Main {
         options.addArguments("--no-sandbox");               // Bypass OS security model
         WebDriver driver = new ChromeDriver(options);
 
-        invalidUserTest(driver);
-        //System.out.println("Result: " + invalidUserTest(driver));
+        if (invalidUserTest(driver) != 0)
+            return;
+        if(recruiterLoginTest(driver) != 0)
+            return;
+        driver.quit();
     }
 
-    private static int invalidUserTest(WebDriver driver)
-    {
+    private static int invalidUserTest(WebDriver driver) {
         driver.get("http://localhost:3000/");
         WebElement element = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div[2]/div/div[1]/button"));
         element.click();
@@ -42,26 +44,75 @@ public class Main {
 
             //Wait 2 seconds for the page to update
             long end = System.currentTimeMillis() + 2000;
-            while (System.currentTimeMillis() < end) {}
+            while (System.currentTimeMillis() < end) {
+            }
 
             //Another try/catch to throw a more useful exception
             try {
                 element = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div[2]/div/form/div[2]/div"));
+            } catch (Exception e) {
+                throw new Exception("FAIL: Invalid User Test");
             }
-            catch(Exception e) {
-                throw new Exception("FAIL: Invalid user test");
+        } catch (Exception e) {
+            System.out.println(e);
+            return -1;
+        }
+
+        System.out.println("PASS: Invalid User Test");
+        return 0;
+    }
+
+    private static int recruiterLoginTest(WebDriver driver) {
+        driver.get("http://localhost:3000/");
+        WebElement element;
+        try {
+            try {
+                element = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div[2]/div/div[2]/button"));
+                element.click();
+                element = driver.findElement(By.xpath("//*[@id=\"email\"]"));       //Find user field
+                element.sendKeys("recruiter@email.com");
+                element = driver.findElement(By.xpath("//*[@id=\"password\"]"));    //Find password field
+                element.sendKeys("password");
+                element = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div[2]/div/form/div[3]/button"));     //Find submit button
+                element.click();                                                                                                //Click submit
+            } catch(Exception e) {
+                throw new Exception("FAIL: Recruiter Login Test -- Login failed");
             }
 
-            if (element != null)
-                System.out.println("PASS: Invalid user test");
-        }
-        catch(Exception e)
-        {
+            long end = System.currentTimeMillis() + 2000;       //Wait 2 sec for page to load
+            while (System.currentTimeMillis() < end) {
+            }
+
+            try {
+                element = driver.findElement(By.xpath("//*[@id=\"618d577d6948161cbb5c6a44\"]"));   //Find job filter
+                element.click();                                                                    //Click job filter
+            } catch(Exception e) {
+                throw new Exception("FAIL: Recruiter Login Test -- click filter failed");
+            }
+
+            try {
+                element = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[2]/div/div/a"));  //Find "learn more"
+                element.click();                                                                            //Click "learn more"
+            } catch(Exception e) {
+                throw new Exception("FAIL: Recruiter Login Test -- click \"Learn more\" failed");
+            }
+
+            try {
+                element = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div"));  //Find email of applicant
+            } catch(Exception e) {
+                throw new Exception("FAIL: Recruiter Login Test -- could not find applicant email");
+            }
+        } catch (Exception e) {
             System.out.println(e);
             driver.quit();
             return -1;
         }
-        driver.quit();
+
+        long end = System.currentTimeMillis() + 1000;       //Wait 1 second before closing
+        while (System.currentTimeMillis() < end) {
+        }
+        System.out.println("PASS: Recruiter Login Test");
         return 0;
     }
+
 }

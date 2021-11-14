ac2cadcd5ff60ff31918c69199f09ef604ce0e94
Christophervtran92 <Christophervtran92@gmail.com>
2021-11-13 22:01:33 -0800

    Added backend script to test creating, updating, and deleting recruiter

--- test-automation/EZApply_Frontend_Tests/src/Main.java
+++ test-automation/EZApply_Frontend_Tests/src/Main.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         System.setProperty("webdriver.chrome.driver", "./src/Selenium/chromedriver");

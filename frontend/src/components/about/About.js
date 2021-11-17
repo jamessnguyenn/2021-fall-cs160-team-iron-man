2f3c5855d170f990395f3f5f7905f174b9f5ebec
smritigurung <smriti.gurung@sjsu.edu>
2021-11-16 21:14:49 -0800

    new about page

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -1,34 +1,128 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import NavBar from './AboutNav'
+import React from "react";
+import NavBar from "./AboutNav";
+import "./About.css";
+import { Row, Col, Card } from "react-bootstrap";
 
 export default function About() {
-    return(
-    
-    <div style={{height:"100vh"}}>
-      <NavBar/>
-    <div className='container'>
-      <Row style={{marginTop: "10vh"}}>
-        <Col className="text-center">
-        <div>
-          <h1 className='' style={{fontSize:"48px"}}>About Us </h1>
+  return (
+    <div>
+      <NavBar />
+      <div className="container mt-5">
+        <div className="about-pic d-flex flex-column justify-content-center align-items-center">
+          <div className="description d-flex flex-column justify-content-center align-items-center text-center">
+            <h1 className="heading">About EZ Apply </h1>
 
-          <h1 className='' style={{fontSize:"48px"}}>About EZ Apply </h1>
-          <h1 className='' style={{fontSize:"24px"}}>Allowing job seekers to apply for jobs easly while giving recruiters easier way to post and recruit new hires  </h1>
-
-          <h1 className='' style={{fontSize:"48px"}}>Vision </h1>
-          <h1 className='' style={{fontSize:"24px"}}>Create economic opportunity for every member of the workforce  </h1>
-
-          <h1 className='' style={{fontSize:"48px"}}>Who are we? </h1>
-          <h1 className='' style={{fontSize:"24px"}}>EZ Apply was created in Fall 2021 by 5 students in CS 160 for a class project. We wanted to make a easy way for people to apply for jobs becuase of the shortange that happened due to COVID-19
-  </h1>
-
-
+            <p className="para">
+              Allowing job seekers to apply for jobs easily while giving
+              recruiters easier way to post jobs and recruit new hires
+            </p>
+            <h1 className="heading">Vision</h1>
+            <p className="para">
+              To simplify and streamline the application process where people
+              can quickly apply for multiple jobs
+            </p>
+          </div>
         </div>
-        
-        </Col>
-      </Row>
+
+        <h1 className="how-to text-center mb-5">How to use EZ Apply</h1>
+
+        <Row
+          xs={1}
+          md={2}
+          className="g-4 mb-5 pb-4"
+          style={{ fontSize: "20px" }}
+        >
+          <Col className="d-flex align-items-stretch">
+            <Card className="card-border" border="info">
+              <Card.Body className="p-5">
+                <Card.Title
+                  className="text-center pb-1"
+                  style={{ fontSize: "22px", fontWeight: "regular" }}
+                >
+                  Getting Started as a Recruiter?
+                </Card.Title>
+                <Card.Text className="guide">
+                  <ol>
+                    <li className="pb-1">
+                      Begin by clicking the "Looking to hire" button on the home
+                      screen. This will redirect you to a login page. If you do
+                      not already have an account, create one with the "Join for
+                      free today".
+                    </li>
+                    <li className="pb-1">
+                      Fill out the required information needed like Name, Email,
+                      Password, Company Name, Company Description, Company Logo,
+                      and Company Website. This will create your account.
+                    </li>
+                    <li className="pb-1">
+                      You will then be taken to the Recruiter Dashboard where
+                      you can view all job positions that you have posted and be
+                      able to select each job position to view all applicants
+                      that have applied.
+                    </li>
+                    <li className="pb-1">
+                      To post jobs for job seekers, click on "Post Job" that
+                      will allow you to fill open job positions at your company.
+                    </li>
+                  </ol>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col className="d-flex align-items-stretch">
+            <Card className="card-border" border="info">
+              <Card.Body className="p-5">
+                <Card.Title
+                  className="text-center pb-1"
+                  style={{ fontSize: "22px", fontWeight: "regular" }}
+                >
+                  Getting Started as a Job Seeker?
+                </Card.Title>
+
+                <Card.Text className="guide">
+                  <ol>
+                    <li className="pb-1">
+                      Begin by clicking the "Looking for a job" button on the
+                      home screen. This will redirect you to a login page. If
+                      you do not already have an account, create one with the
+                      "Join for free today".
+                    </li>
+                    <li className="pb-1">
+                      You will create your profile for when you apply for jobs.
+                    </li>
+                    <li className="pb-1">
+                      You will be taken to the Job Seeker Dashboard that will
+                      show you jobs in a queue-like fashion. You can quickly
+                      click a button to apply or dismiss each job opening within
+                      the queue.
+                    </li>
+                    <li className="pb-1">
+                      You can also view a short snippet (location, position,
+                      company name, short description) of the job positions by
+                      clicking the "Learn more".
+                    </li>
+                  </ol>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        <div className="text-center  pb-5">
+          <h3 className="guide" style={{ fontWeight: "normal" }}>
+            Was this helpful?
+          </h3>
+          <Row>
+            <Col className="d-flex justify-content-end">
+              <div className="btn btn-dark">Yes</div>
+            </Col>
+            <Col className="d-flex justify-content-start">
+              <div className="btn btn-dark">No</div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
